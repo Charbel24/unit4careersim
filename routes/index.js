b2695e443d3f83b1e8ad627a6637022e7cc77d66
Charbel <charbelghosn24@hotmail.com>
2024-08-26 19:39:10 -0700

    endpoints page

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
+const path = require('path')
 const router = require('express').Router()
 const  userRoutes = require('./users')
 const  itemRouter = require('./items')
 const  reviewRouter = require('./reviews')
 const  commentRouter = require('./comments')
 
-
 router.use('/users',userRoutes)
 router.use('/items',itemRouter)
 router.use('/reviews',reviewRouter)
 router.use('/comments',commentRouter)
+
+router.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 module.exports = router;
